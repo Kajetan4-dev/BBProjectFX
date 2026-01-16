0bff35ec5f554544306fb389043a0dc38bafbf3f
Simon <simon.kalkhofer@gmail.com>
2026-01-16 12:40:55 +0100

    Updated all to FX 23 to have everywhere the same Version. Finished quater sircel alignment with the player UI profiles.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
-module org.example {
+module at.ac.hcw.game {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
 
-
     opens at.ac.hcw.Game to javafx.fxml;
+    opens at.ac.hcw.Game.Black_Jack to javafx.fxml;
+    opens at.ac.hcw.Game.Poker_Chips to javafx.fxml;
+
     exports at.ac.hcw.Game;
 }
